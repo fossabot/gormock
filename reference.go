489c10138b7d25ee4340583f4ee8cd66912b3630
Vasiliy Vasilyuk <xorcare@gmail.com>
2018-11-18 00:23:38 +0300

    Establish references to external types

---
+++ reference.go
@@ -0,0 +1,26 @@
+package gormock
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Association type alias.
+type Association = gorm.Association
+
+// Callback type alias.
+type Callback = gorm.Callback
+
+// Dialect type alias.
+type Dialect = gorm.Dialect
+
+// Field type alias.
+type Field = gorm.Field
+
+// JoinTableHandlerInterface type alias.
+type JoinTableHandlerInterface = gorm.JoinTableHandlerInterface
+
+// Scope type alias.
+type Scope = gorm.Scope
+
+// SQLCommon type alias.
+type SQLCommon = gorm.SQLCommon
